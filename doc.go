054aeba0a9e4db0f0e03325b40b01b1d88f121a9
Glib Smaga <glib@uber.com>
2017-03-26 19:07:25 -0700

    Regen doc.go

--- doc.go
+++ doc.go
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 // Copyright (c) 2017 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,56 +18,56 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// Package dig is the Dependency Injection Graph.
+// Package dig is the dig - A dependency injection framework for Go.
 //
 // package dig provides an opinionated way of resolving object dependencies.
 // There are two sides of dig:
-// Register and Resolve.
+// Provide and Resolve.
 //
 // Status
 //
 // BETA. Expect potential API changes.
 //
-// Register
+// Provide
 //
-// Register adds an object, or a constructor of an object to the graph.
+// Provide adds an object, or a constructor of an object to the container.
 //
-// There are two ways to register an object:
+// There are two ways to Provide an object:
 //
-// • Register a pointer to an existing object
+// • Provide a pointer to an existing object
 //
-// • Register a "constructor function" that returns one pointer (or interface)
+// • Provide a "constructor function" that returns one pointer (or interface)
 //
-// Register an object
+// Provide an object
 //
-// Injecting an object means it has no dependencies, and will be used as a
-// **shared** singleton instance for all resolutions within the graph.
+// Registering an object means it has no dependencies, and will be used as a
+// **shared** singleton instance for all resolutions within the container.
 //
 //   type Fake struct {
 //       Name string
 //   }
 //
-//   g := dig.New()
-//   err := g.Register(&Fake{Name: "I am an injected thing"})
+//   c := dig.New()
+//   err := c.Provide(&Fake{Name: "I am an thing"})
 //   require.NoError(t, err)
 //
 //   var f1 *Fake
-//   err = g.Resolve(&f1)
+//   err = c.Resolve(&f1)
 //   require.NoError(t, err)
 //
 //   // f1 is ready to use here...
 //
-// Register a constructor
+// Provide a constructor
 //
 // This is a more interesting and widely used scenario. Constructor is defined as a
 // function that returns exactly one pointer (or interface) and takes 0-N number of
 // arguments. Each one of the arguments is automatically registered as a
 // **dependency** and must also be an interface or a pointer.
 //
-// The following example illustrates injecting a constructor function for type
-// *Object that requires *Dep to be present in the graph
+// The following example illustrates registering a constructor function for type
+// *Object that requires *Dep to be present in the container.
 //
-//   g := dig.New()
+//   c := dig.New()
 //
 //   type Dep struct{}
 //   type Object struct{
@@ -80,31 +78,30 @@
 //     return &Object{Dep: d}
 //   }
 //
-//   err := g.Register(NewObject)
+//   err := c.Provide(NewObject)
 //
 // Resolve
 //
-// Resolve retrieves objects from the graph by type.
+// Resolve retrieves objects from the container by building the object graph.
 //
 // There are future plans to do named retrievals to support multiple
-// objects of the same type in the graph.
+// objects of the same type in the container.
 //
 //
-//   g := dig.New()
+//   c := dig.New()
 //
 //   var o *Object
-//   err := g.Resolve(&o) // notice the pointer to a pointer as param type
+//   err := c.Resolve(&o) // notice the pointer to a pointer as param type
 //   if err == nil {
 //       // o is ready to use
 //   }
 //
 //   type Do interface{}
 //   var d Do
-//   err := g.Resolve(&d) // notice pointer to an interface
+//   err := c.Resolve(&d) // notice pointer to an interface
 //   if err == nil {
 //       // d is ready to use
 //   }
 //
 //
 package dig
->>>>>>> Change order of `make lint` to ensure doc.go header
