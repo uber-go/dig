cba3a3797a540f76ab68927fd83ee8ec280871e7
Abhinav Gupta <mail@abhinavg.net>
2019-10-30 07:35:34 -0700

    Simplify license check
    This drops use of go.uber.org/tools for license checking in favor of a
    shell script similar to Zap's license check.

--- check_license.sh
+++ check_license.sh
@@ -1,23 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -eo pipefail
+ERROR_COUNT=0
+while read -r file
+do
+	case "$(head -1 "${file}")" in
+		*"Copyright (c) "*" Uber Technologies, Inc.")
+			# everything's cool
+			;;
+		*)
+			echo "$file is missing license header."
+			(( ERROR_COUNT++ ))
+			;;
+	esac
+done < <(git ls-files "*\.go")
 
-run_update_license() {
-  # doing this because of SC2046 warning
-  for file in $(git ls-files | grep '\.go$'); do
-    update-license $@ "${file}"
-  done
-}
-
-if [ -z "${DRY_RUN}" ]; then
-  run_update_license
-else
-  DRY_OUTPUT="$(run_update_license --dry)"
-  if [ -n "${DRY_OUTPUT}" ]; then
-    echo "The following files do not have correct license headers."
-    echo "Please run make license and amend your commit."
-    echo
-    echo "${DRY_OUTPUT}"
-    exit 1
-  fi
-fi
+exit $ERROR_COUNT
