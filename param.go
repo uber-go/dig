64bf2b11db0d2ff3e7a71d21385c7f6261df4f2f
Sung Yoon Whang <sungyoon@uber.com>
2021-11-16 01:43:25 -0800

    Remove walkParam

--- param.go
+++ param.go
@@ -81,61 +81,6 @@ func newParam(t reflect.Type, c containerStore) (param, error) {
 	}
 }
 
-// paramVisitor visits every param in a param tree, allowing tracking state at
-// each level.
-type paramVisitor interface {
-	// Visit is called on the param being visited.
-	//
-	// If Visit returns a non-nil paramVisitor, that paramVisitor visits all
-	// the child params of this param.
-	Visit(param) paramVisitor
-
-	// We can implement AnnotateWithField and AnnotateWithPosition like
-	// resultVisitor if we need to track that information in the future.
-}
-
-// paramVisitorFunc is a paramVisitor that visits param in a tree with the
-// return value deciding whether the descendants of this param should be
-// recursed into.
-type paramVisitorFunc func(param) (recurse bool)
-
-func (f paramVisitorFunc) Visit(p param) paramVisitor {
-	if f(p) {
-		return f
-	}
-	return nil
-}
-
-// walkParam walks the param tree for the given param with the provided
-// visitor.
-//
-// paramVisitor.Visit will be called on the provided param and if a non-nil
-// paramVisitor is received, this param's descendants will be walked with that
-// visitor.
-//
-// This is very similar to how go/ast.Walk works.
-func walkParam(p param, v paramVisitor) {
-	v = v.Visit(p)
-	if v == nil {
-		return
-	}
-
-	switch par := p.(type) {
-	case paramSingle, paramGroupedSlice:
-		// No sub-results
-	case paramObject:
-		for _, f := range par.Fields {
-			walkParam(f.Param, v)
-		}
-	case paramList:
-		for _, p := range par.Params {
-			walkParam(p, v)
-		}
-	default:
-		digerror.BugPanicf("received unknown param type %T", p)
-	}
-}
-
 // paramList holds all arguments of the constructor as params.
 //
 // NOTE: Build() MUST NOT be called on paramList. Instead, BuildList
